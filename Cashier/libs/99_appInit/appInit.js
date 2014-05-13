284e551fcb990a065ec02364bdaf7c75d37ec8f4
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2014-05-13 11:47:38 +0200

    Initial structure

---
+++ Cashier/libs/99_appInit/appInit.js
@@ -0,0 +1,6 @@
+(function () {
+    "use strict";
+
+    window.app = window.app || { resolver: {} };
+    app.module = angular.module('cashier', ['ngRoute']);
+})();
---
+++ Cashier/node/config.js
@@ -0,0 +1,37 @@
+var bundler = require("./bundler.js");
+
+exports.initializeBundles = function (webRoot) {
+    bundler.addBundle("~/libs", "js", [
+        {
+            baseDirectory: webRoot + "/",
+            path: "libs",
+            includeSubdirectories: true
+        }
+    ]);
+
+    bundler.addBundle("~/app", "js", [
+        {
+            baseDirectory: webRoot + "/",
+            path: "app",
+            includeSubdirectories: true
+        },
+        {
+            baseDirectory: webRoot + "/",
+            path: "appServices",
+            includeSubdirectories: true
+        }
+    ]);
+
+    bundler.addBundle("~/styles", "css", [
+        {
+            baseDirectory: webRoot + "/",
+            path: "app",
+            includeSubdirectories: true
+        },
+        {
+            baseDirectory: webRoot + "/",
+            path: "assets",
+            includeSubdirectories: true
+        }
+    ]);
+};
