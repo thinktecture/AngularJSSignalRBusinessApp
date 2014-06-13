6131a5ae04ae60d60333ca538a915c8197b60a20
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2014-06-13 14:42:58 +0200

    Backend added

---
+++ Backend/Backend/BillingHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace Backend
+{
+	public class BillingHub : Hub
+	{
+		private const string _ACCOUNTANT_GROUP_NAME = "Accountant";
+
+		public Task RegisterAccountant()
+		{
+			return Groups.Add(Context.ConnectionId, _ACCOUNTANT_GROUP_NAME);
+		}
+
+		public async Task<Guid> AddBillEntry(int productId, int count, decimal price)
+		{
+			Guid id = Guid.NewGuid();
+			await Clients.Group(_ACCOUNTANT_GROUP_NAME).addBillEntry(id, productId, count, price);
+			return id;
+		}
+
+		public void ChangeBillEntryCount(Guid id, int count)
+		{
+			Clients.Group(_ACCOUNTANT_GROUP_NAME).changeBillEntryCount(id, count);
+		}
+
+		public void ChangeBillEntryPrice(Guid id, decimal price)
+		{
+			Clients.Group(_ACCOUNTANT_GROUP_NAME).changeBillEntryPrice(id, price);
+		}
+
+		public void RemoveBillEntry(Guid id)
+		{
+			Clients.Group(_ACCOUNTANT_GROUP_NAME).removeBillEntry(id);
+		}
+	}
+}
---
+++ Backend/Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin.Cors;
+using Owin;
+
+namespace Backend
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.UseCors(CorsOptions.AllowAll);
+			app.MapSignalR();
+		}
+	}
+}
