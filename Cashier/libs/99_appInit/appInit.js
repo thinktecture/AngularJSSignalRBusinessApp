56828e37c25bf55f4ea5e191129d9407597b21b7
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2014-06-30 20:57:03 +0200

    Removed angular strap

--- Cashier/libs/99_appInit/appInit.js
+++ Cashier/libs/99_appInit/appInit.js
@@ -2,7 +2,7 @@
     "use strict";
 
     window.app = window.app || {};
-    app.module = angular.module('cashier', ['ngRoute', 'ngTouch', 'ngAnimate', 'mgcrea.ngStrap']);
+    app.module = angular.module('cashier', ['ngRoute', 'ngTouch', 'ngAnimate']);
 
     app.module.run(function(){
         FastClick.attach(document.body);
